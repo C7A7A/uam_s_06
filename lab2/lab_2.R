875d9d1e11ba470be4f49c599e89824cac8bd68a
C7A7A <czajkamateusz999@mgail.com>
2022-03-10 16:14:45 +0100

    lab2 saved

---
+++ lab2/lab_2.R
@@ -0,0 +1,73 @@
+mean2 = function(values) {
+  sum = 0
+  for (val in values) {
+    sum = sum + val
+  }
+  sum / length(values)
+}
+
+mean3 = function(values) {
+  sum(values) / length(values)
+}
+
+(mean(2:4))
+(mean2(2:5))
+
+
+# odchylenie standardowe
+standardDeviation = function(values) {
+  sum = 0;
+  meanValues = mean(values)
+  
+  for (val in values) {
+    sum = sum + ((meanValues - mean) ^ 2)
+  }
+  
+  sqrt(1 / (length(values) - 1) * sum)
+}
+
+standardDeviation(2:4)
+
+# zmienna dyskretna (ilościowa)
+liczba_bledow = c(1, 1, 2, 0, 1, 3, 1, 4, 4, 4, 0, 1, 0, 0, 0, 2, 3,
+                  4, 0, 1, 5, 2, 3, 5, 3, 2, 2, 4, 0, 2, 2, 0, 2, 2,
+                  3, 3, 1, 3, 2, 2, 0, 0, 5, 4, 2, 1, 5, 2, 2, 0)
+
+# zmienna ciągła
+czas_oczekiwania = c(4.03, 11.04,  5.73, 12.36, 13.17,  0.64,  7.39, 12.49,  7.72,  6.39, 13.40,  6.35,  9.49,  8.02,  
+                     1.44, 12.60,  3.45,  0.59,  4.59, 13.36, 12.45,  9.70,  8.97, 13.92,  9.18,  9.92,  7.62,  8.32,  
+                     4.05,  2.06, 13.48, 12.63,  9.67, 11.14,  0.34,  6.69, 10.62,  3.03,  4.45, 3.24,  2.00,  5.80,  
+                     5.79,  5.16,  2.13,  1.94, 3.26,  6.52,  3.72, 12.01,  0.64,  6.19, 11.18,  1.71,  7.85,  2.89,  
+                     1.79, 10.55, 12.53,  5.24,  9.31,  1.33,  5.38,  3.84, 11.40,  6.28, 11.34, 11.37, 11.12, 6.16,
+                     10.56,  8.81,  9.94,  0.01,  6.65,  3.08,  5.32,  8.58,  4.93,  1.56,  3.41,  9.35,  5.85, 11.03,  
+                     1.44,  6.09, 13.79, 12.50, 12.41,  2.45,  1.83,  9.14, 4.81,  9.19,  4.49,  2.63, 10.95,  1.31,  6.53, 7.16)
+
+
+mean(liczba_bledow)
+var(liczba_bledow)
+sd(liczba_bledow)
+
+plot(liczba_bledow)
+table(liczba_bledow)
+plot(table(liczba_bledow))
+barplot(liczba_bledow)
+barplot(table(liczba_bledow))
+
+prop.table(liczba_bledow)
+barplot(prop.table(table(liczba_bledow)))
+
+pie(prop.table(table(liczba_bledow)))
+pie(table(liczba_bledow))
+
+
+
+
+
+
+
+
+
+
+
+
+
